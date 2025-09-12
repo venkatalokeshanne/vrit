ea0fdc2b21a58ee6bebae0948cf7ebb84e817bc5
Venkata Lokesh Anne <annevlokesh@gmail.com>
2025-09-12 10:55:00 +0200

    Tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -133,8 +133,8 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en-US">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* Google Tag Manager - Move to head but change strategy */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
